040316c12653b90e1d8c5fb5301f80dbcd6bc68d
judith2702 <87411824+judith2702@users.noreply.github.com>
2022-02-15 17:14:44 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "judith.soundarya.joseph@pwc.com"
     AccountName               = "sandbox-aft"
     ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    SSOUserEmail              = "judith.soundarya.joseph@pwc.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
